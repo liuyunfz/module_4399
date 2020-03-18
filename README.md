c1d0f687a9a4f34da08fd5fb30e2ea19e21c3f8d
liuyunfz <liuyunfz@gmail.com>
2020-03-18 13:50:55 +0800

    大量修改函数返回信息，并添加了函数说明

--- README.md
+++ README.md
@@ -10,21 +10,44 @@
 - ~~4399群组hash验证~~
 - ~~4399群组帖子鲜花~~
 - ~~4399群组关注~~<br>
-## **已使用的Python库**
+## **第三方Python库**
 - requests
-- json
-- urllib
 - lxml
-- time
-- ~~sqlite3~~<br>
-## **已存在于发布在第三方平台上的教程**(需要下载手机app查看)
+<br>
+## **已存在于发布在第三方平台上的教程**
 - [4399登录详解](https://www.coolapk.com/feed/13068295?shareKey=YWFjNWViNjYxYTRhNWQ5NTYxNmE~&shareUid=1256119&shareFrom=com.coolapk.market_9.5)
 - [利用cookie爬取4399单个群组的用户数据](https://www.coolapk.com/feed/13102437?shareKey=MzA5Y2ZmNmI3YTc5NWQ5NTY2MmY~&shareUid=1256119&shareFrom=com.coolapk.market_9.5)
 - [爬取4399全站用户](https://www.coolapk.com/feed/13180495?shareKey=ZGFmODg4ZWIwM2E5NWQ5NTY2NzQ~&shareUid=1256119&shareFrom=com.coolapk.market_9.5)
-- [4399HASH实战破解](http://blog.6yfz.cn/2019/08/28/%E5%AE%9E%E6%88%984399%E7%A0%B4%E8%A7%A3HASH%E9%AA%8C%E8%AF%81/)   (发布于自己博客，可直接查看)<br>
+- [4399HASH实战破解](http://blog.6yfz.cn/%E5%AE%9E%E6%88%984399%E7%A0%B4%E8%A7%A3HASH%E9%AA%8C%E8%AF%81.html)   <br>
+## **功能解析**
+### 1. 4399登录
+方法名:sign_in  
+> |需要参数|参数类型|说明                              |
+|:-----   |:------|:-----------------------------   |
+|username   |string    |登录账户的用户名   |
+|password  |string | 登录账户的密码                      |
+|captcha |string |验证码，可空                         |
+  
+返回数据（type:json）  
+> |key|说明                              |
+|:-----  |:-----------------------------   |
+|id  |登录状态返回，0-登录成功  ；1-需要验证码  ；2-其他错误，通过msg返回具体内容   |
+|msg  | 登录的具体状态描述                     |
+|userid|用户的id，仅在登录成功时返回                         |
+|username|用户的论坛名，仅在登录成功时返回                         |
+|cookie|账号登录cookie，仅在登录成功时返回                         |
+  
+### 2. 4399群组首页签到
+方法名:daily_sign  
+> |需要参数|参数类型|说明                              |
+|:-----   |:------|:-----------------------------   |
+|cookie   |string    |需要签到用户的账户cookie，可通过登录函数返回   |
+  
+返回数据（type:json）  
+返回的为官方json，请自行尝试分析。
+### 3. 剩余功能，下次添加
 ## **展望**
 ⭐⭐⭐
 未来会以md教程的方式实现4399群组的全站爬取
 ⭐⭐⭐
-
 
--- module_4399.py
+++ module_4399.py
@@ -1,65 +1,51 @@
 # -*- coding: utf-8 -*-
-import requests,json,time,sqlite3
+import requests,json,time
 from lxml import etree
 from urllib import parse
-#链接数据库
-def link_db(uid,name,pw,cookie):
-    cnt = sqlite3.connect('C:/Users/ly/Desktop/py_temporary/account_4399/account_4399.db')
-    cur = cnt.cursor()#创建游标
-    #print(cur.execute(".tables"))
-    print(cur.execute("INSERT INTO MAIN VALUES ({0},'{1}','{2}','{3}');".format(uid,name,pw,cookie)))
-    #cnt.execute("DELETE from MAIN where UID=2968726696;")
-    account = cur.execute("SELECT * FROM MAIN;")
-    #print(cur.fetchall())
-    for row in account:
-        print ("UID = ", row[0])
-        print ("USERNAME = ", row[1])
-        print ("PASSERWORD = ", row[2])
-        print ("COOKIE = ", row[3], "\n")
-    cnt.commit()
+
 #判断登录状态
-def judge_sign_data(data):
+def __judge_sign_data(data):
     selector = etree.HTML(data)
     if selector.xpath("//*[@id='Msg']") != []:
         sign_error =selector.xpath("//*[@id='Msg']")
-        print(sign_error[0].text.strip())
-        return 1
+        #print(sign_error[0].text.strip())
+        return {"id":2,"msg":sign_error[0].text.strip()}
     elif selector.xpath("/html/body/script[2]") != []:
-        print("登录成功")
-        return 0
+        #print("登录成功")
+        return {"id":0,"msg":"登录成功"}
     else:
-        print("需要验证码")
-        return 2
+        #print("需要验证码")
+        return {"id":1,"msg":"需要验证码"}
 #登录账号，返回uid与cookie
-def sign_in(passername,passerword,captcha=""):
-    data=r"loginFrom=uframe&postLoginHandler=default&layoutSelfAdapting=true&externalLogin=qq&displayMode=undefined&layout=vertical&appId=my&gameId=&css=%2F%2Fs1.img4399.com%2Fbase%2Fcss%2Fptlogin.css%3F431c76e&redirectUrl=&sessionId=captchaReqddf4cf8217f50404217&mainDivId=popup_login_div&includeFcmInfo=false&userNameLabel=4399%E7%94%A8%E6%88%B7%E5%90%8D&userNameTip=%E8%AF%B7%E8%BE%93%E5%85%A54399%E7%94%A8%E6%88%B7%E5%90%8D&welcomeTip=%E6%AC%A2%E8%BF%8E%E5%9B%9E%E5%88%B04399&username={0}&password={1}&inputCaptcha={2}".format(passername,passerword,captcha)
+def sign_in(username,password,captcha=""):
+    data=r"loginFrom=uframe&postLoginHandler=default&layoutSelfAdapting=true&externalLogin=qq&displayMode=undefined&layout=vertical&appId=my&gameId=&css=%2F%2Fs1.img4399.com%2Fbase%2Fcss%2Fptlogin.css%3F431c76e&redirectUrl=&sessionId=captchaReqddf4cf8217f50404217&mainDivId=popup_login_div&includeFcmInfo=false&userNameLabel=4399%E7%94%A8%E6%88%B7%E5%90%8D&userNameTip=%E8%AF%B7%E8%BE%93%E5%85%A54399%E7%94%A8%E6%88%B7%E5%90%8D&welcomeTip=%E6%AC%A2%E8%BF%8E%E5%9B%9E%E5%88%B04399&username={0}&password={1}&inputCaptcha={2}".format(username,password,captcha)
     try:
         rsp = requests.post("http://ptlogin.4399.com/ptlogin/login.do?v=1",data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36","Host": "ptlogin.4399.com","Accept-Encoding": "gzip, deflate","Referer":"http://ptlogin.4399.com/ptlogin/loginFrame.do?postLoginHandler=default&redirectUrl=&displayMode=popup&css=&appId=www_home&gameId=&username=liuyunfz&externalLogin=qq&password=&mainDivId=popup_login_div&autoLogin=false&includeFcmInfo=false&qrLogin=true&v=1564145136161","Connection":"keep-alive","Cache-Control":"max-age=0","Origin":"http://ptlogin.4399.com","Upgrade-Insecure-Requests":"1","Content-Type":"application/x-www-form-urlencoded","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3","Accept-Language":"zh-CN,zh;q=0.9","Cookie":r"UM_distinctid=16be1237f3d330-0026f1ba77007-e343166-1fa400-16be1237f3e8d7; _gprp_c=\"\"; Qnick=; _4399tongji_vid=156311428638372; _4399stats_vid=15631142873775485; Hm_lvt_334aca66d28b3b338a76075366b2b9e8=1562850329,1563114258,1563190247; ptusertype=my.4399_login; Puser=liuyunfz; Xauth=80b197113153cacc4d59b638f294d8a2; Pnick=%E9%A5%B0%E9%9D%9E; Uauth=4399\|1\|2019718\|my.#my.my\|1563421044972\|fd0b9e06776a854c6a80194081f5ea00; home4399=yes; USESSIONID=ec2acf31-7106-4f84-a85e-32a89421c6dd"},timeout=3)
-        #print(rsp.text)
-        if judge_sign_data(rsp.text) == 0:
+        status = __judge_sign_data(rsp.text)
+        if status["id"] == 0:
             global cookieStr,username,userid
             cookieStr = ''        
             for item in rsp.cookies:
-                #print(item.name,"   :    ",item.value)
                 cookieStr = cookieStr + item.name + '=' + item.value + ';'
             username = parse.unquote(rsp.cookies["Pnick"])
             Pauth = rsp.cookies["Pauth"]
             split_pauth = Pauth.split("|")
             userid = split_pauth[0]
-            return [userid,cookieStr]
+            return {"id":0,"msg":"登录成功",'userid':userid,'username':username,'cookie':cookieStr}
+        elif status['id'] == 1:
+            return {"id":1,"msg":"需要验证码"}
         else:
-            return 1
+            return {"id":2,"msg":status['msg']}
         
     except TimeoutError:
-        print("time faild")
-        return 2
+        #print("time faild")
+        return {"id":3,"msg":"timeout"}
     except:
-        print("sign error")       
-        return 3
+        #print("sign error")       
+        return {"id":4,"msg":"something error"}
 #群组首页签到
 def daily_sign(cookie):
     url = "http://my.4399.com/plugins/sign/set-t-{0}".format(int(time.time()*1000))
-    print(url)
     try:
         sign_headers={
         "Accept":"application/json, text/javascript, */*; q=0.01",
@@ -73,10 +59,7 @@ def daily_sign(cookie):
         }
         rsp = requests.get(url,headers=sign_headers)
         sign_json = json.loads(rsp.text)
-        if sign_json["code"] == 100:
-            return 0
-        else:
-            return sign_json["result"]
+        return sign_json
 
     except:
         return 1
@@ -97,8 +80,7 @@ def information_grade(cookie):
         }
         rsp = requests.get(url=url,headers=headers,timeout=3)       
         rsp_grade = json.loads(rsp.text)
-        #print(rsp_grade["code"])
-        
+        '''
         if rsp_grade["code"] == 100:            
             grade = rsp_grade["result"]["grade"]
             credit = rsp_grade["result"]["credit"]
@@ -109,13 +91,15 @@ def information_grade(cookie):
         else:
             return 1 
             #print("grade error") 
+        '''
+        return rsp_grade
     except:
         return 2
         #print("information_grade error")
 #获取粉丝与关注等信息
 def information_fans(uid):
     try:
-        url="http://my.4399.com/forums/user-info?uid={0}&withMedal=1&_AJAX_=1".format(userid)
+        url="http://my.4399.com/forums/user-info?uid={0}&withMedal=1&_AJAX_=1".format(uid)
         headers={
             "Host":"my.4399.com",
             "Upgrade-Insecure-Requests":"1",
@@ -124,19 +108,23 @@ def information_fans(uid):
             "Accept-Encoding":"gzip, deflate",
             "Accept-Language":"en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
             "Cache-Control":"max-age=0",
+            "Cookie":"",
             "Connection":"keep-alive"
         }
         rsp = requests.get(url=url,headers=headers)
         rsp_json = json.loads(rsp.text)
+        #print(rsp_json)
         if rsp_json["code"] == 100:            
-            followed = rsp_json["result"]["num_been_followed"]
-            num_feed = rsp_json["result"]["num_feed"]
-            avatar = rsp_json["result"]["avatar"]
-            num_follow = rsp_json["result"]["num_follow"]                    
-            return([followed,num_follow,num_feed,avatar])
+            followed = rsp_json.get("result").get("num_been_followed")
+            num_feed = rsp_json.get("result").get("num_feed")
+            avatar = rsp_json.get("result").get("avatar")
+            num_follow = rsp_json.get("result").get("num_follow")                  
+            return {"id":0,'followed':followed,'num_follow':num_follow,'num_feed':num_feed,'avatar':avatar}
         else:
-            return 1
+            return {"id":1,'result':rsp_json}
             #print("fans error")
+        
+        #return rsp_json
     except:
         return 2
         #print("information_fans error")
